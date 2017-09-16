bb16382eda1cf5f87ae8b0c3af8f39c68bd9dcc7
梁晓庆 <liangxiaoqing@meituan.com>
2017-09-16 22:43:37 +0800

    fix bug

--- ezhihui-api/src/main/java/com/ezhihui/www/api/controller/WeChatController.java
+++ ezhihui-api/src/main/java/com/ezhihui/www/api/controller/WeChatController.java
@@ -1,9 +1,13 @@
 package com.ezhihui.www.api.controller;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
 
 /**
  * Created by lxq on 2017/9/16.
@@ -15,7 +19,19 @@ public class WeChatController {
 
     @RequestMapping(value = "/getToken", method = RequestMethod.GET)
     @ResponseBody
-    public String getToken() {
-        return WECHAT_TOEN;
+    public String getToken(@RequestParam("signature") String signature, @RequestParam("echostr") String echostr, @RequestParam("timestamp") String timestamp, @RequestParam("nonce") String nonce) {
+
+        String[] arr = new String[]{WECHAT_TOEN, timestamp, nonce};
+        Arrays.sort(arr);
+
+        String content = arr[0] + arr[1] + arr[2];
+
+        String temp = DigestUtils.shaHex(content);
+
+        if (temp.equalsIgnoreCase(signature)) {
+            return echostr;
+        }
+
+        return "wechat_token_error";
     }
 }
